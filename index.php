5edd99c8b7041571b39d6dd2bc06d801132173dc
testuser1000 <athar_tahir2003@hotmail.com>
2015-02-19 18:02:21 +0000

    Update index.php

--- index.php
+++ index.php
@@ -1,24 +1,36 @@
 <html>
 <head>
 	<title>PHP Task</title>
+	<script type="text/javascript" src="jquery.js"></script>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+
 </head>
 <body>
-	<form action="usearch.php" method="post">
-		<h2>This is a Movie Website</h2>
-			<p>Search for the Movie Related Content Here</p>
-				Search: <input type="text" name="usearch"><br>
-		<input type="submit">
-	</form>
-
-	<form action="msearch.php" method="post">
-				Search for Movie: <input type="text" name="msearch"><br>
-		<input type="submit">
-	</form>
 	
-	<form action="asearch.php" method="post">
-				Search for actor: <input type="text" name="asearch"><br>
+	<div class="container">
+	
+	<header class="jumbotron">
+		<ul class="nav nav-pills">
+			<li role="presentation" class="active"><a href="index.php">Home</a></li>
+			<li role="presentation"><a href="">About</a></li>
+			<li role="presentation"><a href="">Contact</a></li>
+		</ul>
+	
+	<h2>This is a Movie Website</h2>
+	
+	<form action="search.php" method="post">
+			<p>Search for the Movie Related Content Here</p>
+				Search: <input type="text" name="search">
 		<input type="submit">
 	</form>
+	</header>
 
+
+	<div class="content"> 
+
+	</div>
+</div><!-- Container Ends-->
 </body>
 </html>
